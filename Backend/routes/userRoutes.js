92eb212423e7d96e3011667cec65e18f3cfd5e00
Manak Agarwal <itsmemanak94@gmail.com>
2020-08-12 14:30:19 +0530

    Add files via upload

---
+++ Backend/routes/userRoutes.js
@@ -0,0 +1,67 @@
+import express from 'express';
+import User from '../models/userModel'
+const router = express.Router();
+import { getToken } from '../util';
+router.post("/signin", async (req, res) => {
+    
+    const signinUser = await User.findOne({
+        email: req.body.email,
+        password: req.body.password
+    });
+
+    if (signinUser) {
+        res.send({
+            _id: signinUser.id,
+            name: signinUser.name,
+            email: signinUser.email,
+            isAdmin: signinUser.isAdmin,
+            token: getToken(signinUser)
+
+        });
+    }
+    else {
+        res.status(401).send({ message: 'Invalid Email or Password.' });
+    }
+});
+
+router.post('/register', async (req, res) => {
+    const user = new User({
+      name: req.body.name,
+      email: req.body.email,
+      password: req.body.password,
+    });
+    const newUser = await user.save();
+    if (newUser) {
+      res.send({
+        _id: newUser.id,
+        name: newUser.name,
+        email: newUser.email,
+        isAdmin: newUser.isAdmin,
+        token: getToken(newUser),
+      });
+    } else {
+      res.status(401).send({ message: 'Invalid User Data.' });
+    }
+  });
+
+
+
+router.get("/createadmin", async (req, res) => {
+    try {
+        const user = new User({
+            name: 'Manak',
+            email: 'mnkgrwl94@gmail.com',
+            password: '1234',
+            isAdmin: true
+        });
+        const newUser = await user.save();
+        res.send(user);    
+    }
+    catch (error) {
+        res.send({ msg: error.message });
+    }
+    
+    
+})
+
+export default router;
